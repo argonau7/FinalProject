2eb0691208bed017c5dd5b359f07f4664ff40fd5
Lukas <lukas.zuegg@gmail.com>
2020-05-09 15:11:35 +0200

    javascript pulls correctly

--- FinalProject.js
+++ FinalProject.js
@@ -2,7 +2,12 @@ async function loadMovies() {
 let response = await fetch("http://www.omdbapi.com?t=joker&apikey=91b6dc90")
 
 let movies = await response.json();
-return movies.poster
-
+return movies.Poster
+//console.log(movies.Poster)
 }
 
+//async function showpic() {
+
+//document.getElementById('imageone').src = loadMovies(); 
+
+//}
