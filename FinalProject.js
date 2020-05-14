a910a6b477ae698d347d7b347f59cb5733827038
Luk <ss>
2020-05-14 09:04:13 +0200

    Onloadworks

--- FinalProject.js
+++ FinalProject.js
@@ -1,21 +1,21 @@
 async function loadMovies() {
-    let response = await fetch("http://www.omdbapi.com/?s=Batman&apikey=91b6dc90")
+    let response = await fetch("http://www.omdbapi.com/?s=love&apikey=91b6dc90")
 
     let movies = await response.json();
     // below 'return' works and loads the poster but provides no console output but not needed once it works
    // return movies.Poster
     // console.log was useful to check if loadMovies() actually fetched anyhing but not for below uri so now deactivated
     //console.log(movies.Search[1].Poster)
-    return movies.Search[1].Poster
+    return movies.Search[6].Poster
    //console.log(movies) //this works
     
 }
 
 let img1 = document.querySelector('#imageone')
-let btn1 = document.querySelector('#btn1')
-
-btn1.addEventListener('click',() => {
-    loadMovies().then(uri => img1.src = uri).catch(err => console.log(err.message))
-    
-})
+//let btn1 = document.querySelector('#btn1')
+//btn1.addEventListener('click',() => {
+    //holy shxt, this works
+    window.onload = function() {
+    loadMovies().then(uri => img1.src = uri).catch(err => console.log(err.message))   
+}
 
