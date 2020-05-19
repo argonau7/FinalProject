772a323cdc266ad8b50ac9e1f307355f928f1625
Luk <ss>
2020-05-19 14:00:34 +0200

    some mods

--- FinalProject.js
+++ FinalProject.js
@@ -1,24 +1,21 @@
 async function loadMovies() {
     let response = await fetch("http://www.omdbapi.com/?s=love&apikey=91b6dc90")
-
     let movies = await response.json();
-    // below 'return' works and loads the poster but provides no console output but not needed once it works
-   // return movies.Poster
-    // console.log was useful to check if loadMovies() actually fetched anyhing but not for below uri so now deactivated
-    //console.log(movies.Search[1].Poster)
-    return movies.Search[6].Poster
+    // return movies.Poster
+    // console.log(movies.Poster) //this works and shows poster url with single return item 
+    return movies.Search[0].Poster   // required when multiple results
+    //for (var i =0; i< 9 ;i++) {
+    //let obj = objArray.find(obj => obj.id == Poster);
+    //}
 
-   //console.log(movies) //this works
-    
 }
 
-let img1 = document.querySelector('#imageone')
 //let btn1 = document.querySelector('#btn1')
-
-
 //btn1.addEventListener('click',() => {
-    //holy shxt, this works
-    window.onload = function() {
-    loadMovies().then(uri => img1.src = uri).catch(err => console.log(err.message))   
+
+let img1 = document.querySelector('#imageone')
+//holy shxt, this works
+window.onload = function () {
+    loadMovies().then(uri => img1.src = uri).catch(err => console.log(err.message))
 }
 
