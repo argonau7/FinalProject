0ad6b0034a955cf230717844fd74e4b7b36834f3
Luk <ss>
2020-05-19 16:22:01 +0200

    keef

--- FinalProjectEX.js
+++ FinalProjectEX.js
@@ -1,20 +1,30 @@
 async function loadMovies() {
-    let response = await fetch("http://www.omdbapi.com/?s=Batman&apikey=91b6dc90")
-
+    let response = await fetch("http://www.omdbapi.com/?s=love&apikey=91b6dc90")
     let movies = await response.json();
-    // below 'return' works and loads the poster but provides no console output but not needed once it works
-   return movies.data
-    // console.log was useful to check if loadMovies() actually fetched anyhing but not for below uri so now deactivated
-    //flexing - this also works and probably better for looping[]
-    //return movies.Search[4].Poster
-   //console.log(movies) //this works
     
+    // What's in movies
+    console.log("Here's the movies variable: ");
+    console.log(JSON.stringify(movies)) //
+    
+    // Looks like we actually want the `Search` property:
+    console.log("This should be the array:")
+    console.log(JSON.stringify(movies.Search))
+
+    // Return the array
+    return movies.Search
 }
 
-let img1 = document.querySelector('#imageone')
-let btn1 = document.querySelector('#btn1')
+async function  main() {
+  // Load movies
+  const moviesArray = await loadMovies()
 
-window.onload = function() {
-    loadMovies().then(uri => img1.src = uri).catch(err => console.log(err.message))
+  // Iterate through every movie in the array
+  for (var index = 0; index < moviesArray.length; index++) {
+    // Grab a reference to the movie at `index`
+    const movie = moviesArray[index];
+
+    // Print it for fun.
+    console.log("The movie object at index " + index + " is:");
+    console.log(JSON.stringify(movie))
+  }
 }
-
