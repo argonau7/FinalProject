24c65326d84c1c31563b6d3a7199a500e3ed3873
Lukas <lukas.zuegg@gmail.com>
2020-05-20 21:49:41 +0200

    now loop works

--- FinalProjectEX.js
+++ FinalProjectEX.js
@@ -1,30 +1,32 @@
 async function loadMovies() {
-    let response = await fetch("http://www.omdbapi.com/?s=love&apikey=91b6dc90")
-    let movies = await response.json();
-    
-    // What's in movies
-    console.log("Here's the movies variable: ");
-    console.log(JSON.stringify(movies)) //
-    
-    // Looks like we actually want the `Search` property:
-    console.log("This should be the array:")
-    console.log(JSON.stringify(movies.Search))
+  let response = await fetch("http://www.omdbapi.com/?s=love&apikey=91b6dc90")
+  let movies = await response.json();
+  
+  // // What's in movies
+  // console.log("Here's the movies variable: ");
+  // console.log(JSON.stringify(movies)) //
+  
+  // // Looks like we actually want the `Search` property:
+  // console.log("This should be the array:")
+  // console.log(JSON.stringify(movies.Search))
 
-    // Return the array
-    return movies.Search
+  // Return the array
+  return movies.Search
 }
 
 async function  main() {
-  // Load movies
-  const moviesArray = await loadMovies()
+// Load movies
+const moviesArray = await loadMovies()
 
-  // Iterate through every movie in the array
-  for (var index = 0; index < moviesArray.length; index++) {
-    // Grab a reference to the movie at `index`
-    const movie = moviesArray[index];
+// Iterate through every movie in the array
+for (var index = 0; index < moviesArray.length; index++) {
+  // Grab a reference to the movie at `index`
+  const movie = moviesArray[index];
+  const poster = movie.Poster
+  console.log(poster)
 
-    // Print it for fun.
-    console.log("The movie object at index " + index + " is:");
-    console.log(JSON.stringify(movie))
-  }
+  // Print it for fun.
+  //console.log("The movie object at index " + index + " is:");
+  //console.log(JSON.stringify(movie))
+}
 }
